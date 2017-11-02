6e87c7f342b27de9eec70a8bb4f08e4c5f144415
Nux007 <007.nux@gmail.com>
2017-11-02 07:55:25 +0100

    * Slowly switching to XML Xbmc gui.

--- addon.py
+++ addon.py
@@ -6,99 +6,53 @@ from resources.lib import EPGXML
 
 
 
-class EPGAddonWindow(xbmcgui.WindowDialog):
+'''
+Global class handling EPG Gui.
+'''
+class XMLWindowEPG(xbmcgui.WindowXMLDialog):
     
-    channels_list = [];
+    # XML gui structure.
     
-    # Layout purpose.
-    base_factor = 100
-    y_factor    = 50
         
-    def __init__(self, channels_list_data, *args, **kwargs):  
-        
-        if channels_list_data != None and len(channels_list_data) > 0:
-            self.channels_list = channels_list_data
-            
-        # Base layout.
-        self.addControl(xbmcgui.ControlImage(0, 0, 1300, 720, 'special://home//addons//plugin.program.super.favourites.xmltv//resources//media//background.jpg'))      
+    def __init__(self, strXMLname, strFallbackPath):
+        xbmcgui.WindowXML.__init__(self, strXMLname, strFallbackPath, default='Default', defaultRes='720p', isMedia=True)
 
-        # For gui tests purpose only.
-        self.__addTestsControls()
 
-        if len(self.channels_list) > 0 :
-            for i in self.channels_list:
-                self.addControl(i)
+    def onInit(self):
+        pass
 
 
 
     def onAction(self, action):
-        # Close the current window
+        
+        # Same as normal python Windows.
         if action == xbmcgui.ACTION_PREVIOUS_MENU:
+            xbmc.sleep(500)
             self.close()
         
         # Select an EPG line into the window
-        if action == xbmcgui.ACTION_MOUSE_LEFT_CLICK or action == xbmcgui.ACTION_SELECT_ITEM:
-            pass
-
-
-    def onControl(self, control):
-        if control == self.list:
-            item = self.list.getSelectedItem()
-            self.message('You selected : ' + item.getLabel())
-            self.close()
+        #if action == xbmcgui.ACTION_MOUSE_LEFT_CLICK or action == xbmcgui.ACTION_SELECT_ITEM:
+        #    pass
 
 
 
-    def message(self, message):
-        dialog = xbmcgui.Dialog()
-        dialog.ok(" My message title", message)
+    def onClick(self, controlID):
+        pass
         
-    
-    
-    # The only goal of this function is to test the graphical layout, it will be commented out later on.
-    def __addTestsControls(self):
-        self.button0 = xbmcgui.ControlButton(-450, 100, 2200, 100, 'Web TV 1', alignment=6)
-        self.addControl(self.button0)
+       
 
-        self.button1 = xbmcgui.ControlButton(-450, 150, 2200, 100, 'Web TV 2', alignment=6)
-        self.addControl(self.button1)
-
-        self.button2 = xbmcgui.ControlButton(-450, 200, 2200, 100, 'Web TV 3', alignment=6)
-        self.addControl(self.button2)
-
-        self.button3 = xbmcgui.ControlButton(-450, 250, 2200, 100, 'Web TV 4', alignment=6)
-        self.addControl(self.button3)
-
-        self.button4 = xbmcgui.ControlButton(-450, 300, 2200, 100, 'Web TV 5', alignment=6)
-        self.addControl(self.button4)
-        
-                
+    def onFocus(self, controlID):
+        pass
+            
 
 ''''''''''''''''''''''''''''''''''''''''''
 ''''''''''''''''''''''''''''''''''''''''''    
     
-
-def main():
+# Basic addons information.
+addonid      = 'plugin.program.super.favourites.xmltv'
+settings     = xbmcaddon.Addon(addonid)
     
-    # Basic addons information.
-    addonid      = 'plugin.program.super.favourites.xmltv'
-    settings     = xbmcaddon.Addon(addonid)
-    addonName    = settings.getAddonInfo('name')
-    addonVersion = settings.getAddonInfo('version')
-    addonHandle  = int(sys.argv[1])
+EPGgui = XMLWindowEPG('epg.xml', settings.getAddonInfo('path'))
+EPGgui.doModal()
     
-    # Getting arguments.
-    params = urlparse.parse_qs('&'.join(sys.argv[1:]))
-    
-    # Gettinh EPG xml files infos
-    epg_list = EPGXML.getEPGControlsList()
-    
-    # Creatin EPG window
-    epg_window = EPGAddonWindow(epg_list, **params)
-    epg_window.doModal()
-    del epg_window
-
-
-if __name__ == '__main__':
-    main()
-
+del EPGgui
